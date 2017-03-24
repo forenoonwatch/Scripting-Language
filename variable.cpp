52b32ddb08441781461479bca1c77aee72ae3b0c
forenoonwatch <91013521+forenoonwatch@users.noreply.github.com>
2017-03-24 09:23:37 -0400

    added more to variable.cpp

--- variable.cpp
+++ variable.cpp
@@ -23,17 +23,30 @@ void Variable::add(const Variable& a, const Variable& b, Variable& out) {
 	else if (a.type == VariableType::INT) {
 		out.type = VariableType::INT;
 
-		if (b.type == VariableType::STRING) {
-			out.stringValue = a.intValue + b.stringValue;
-		}
-		else if (b.type == VariableType::INT) {
-			out.stringValue = a.intValue + b.intValue;
+		if (b.type == VariableType::INT) {
+			out.intValue = a.intValue + b.intValue;
 		}
 		else if (b.type == VariableType::FLOAT) {
-			out.stringValue = a.intValue + b.floatValue;
+			out.intValue = a.intValue + b.floatValue;
 		}
 		else {
 			// TODO: runtime error
 		}
+	}
+	else if (a.type == VariableType::FLOAT) {
+		out.type = VariableType::FLOAT;
+
+		if (out.type == VariableType::INT) {
+			out.floatValue = a.floatValue + b.intValue;
+		}
+		else if (out.type == VariableType::FLOAT) {
+			out.floatValue = a.floatValue + b.floatValue;
+		}
+		else {
+			// TODO: runtime error
+		}
+	}
+	else {
+		// TODO: runtime error
 	}
 }
